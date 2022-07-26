ff3f681d3c17f0fb67c3c728c31b6f55d9976b9f
e.baxter@dark-coin.io <e.baxter@dark-coin.io>
2022-07-26 18:34:20 -0500

    Updating application to exit and refund coins in the event of a timeout.

--- index.mjs
+++ index.mjs
@@ -1,43 +1,54 @@
-import { loadStdlib } from '@reach-sh/stdlib'; //Imports reach standard library loader
-import * as backend from './build/index.main.mjs'; //Imports backend which ./reach compile will produce
-const stdlib = loadStdlib(); // loads the standard library dynamically based on the REACH_CONNECTOR_MODE environment variable
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
 
-const startingBalance = stdlib.parseCurrency(100); // defines a quantity of network tokens as the starting balance for each test account
-const accAlice = await stdlib.newTestAccount(startingBalance); // create test accounts w/ initial endowments for Alice and Bob. This will only work on the Reach provided developer testing network.
+const startingBalance = stdlib.parseCurrency(100);
+const accAlice = await stdlib.newTestAccount(startingBalance);
 const accBob = await stdlib.newTestAccount(startingBalance);
 
-const fmt = (x) => stdlib.formatCurrency(x, 4); // shows a helpful function for displaying currency amounts with up to 4 decimal places.
-const getBalance = async (who) => fmt(await stdlib.balanceOf(who)); // shows a helpful function for getting the balance of a participant and displaying it up to 4 decimal places.
-const beforeAlice = await getBalance(accAlice); // This & the next line get the balance before the game starts for both Alice and Bob
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
 const beforeBob = await getBalance(accBob);
 
-const ctcAlice = accAlice.contract(backend); // Alice deploys the application
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo()); // Bob attaches to application
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
-const HAND = ['Rock', 'Paper', 'Scissors']; // define arrays to hold the meaning of the hands and outcomes.
+const HAND = ['Rock', 'Paper', 'Scissors'];
 const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
-const Player = (Who) => ({ // defines a constructor for the player implementation
-  ...stdlib.hasRandom,  
-  getHand: () => { //  implement the getHand method
-        const hand = Math.floor(Math.random() * 3);
-        console.log(`${Who} played ${HAND[hand]}`);
-        return hand;
-    },
-seeOutcome: (outcome) => { // implement the seeOutcome method
+const Player = (Who) => ({
+  ...stdlib.hasRandom,
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
     console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
-},
+  },
+  informTimeout: () => {
+    console.log(`${Who} observed a timeout`);
+  },
 });
 
-await Promise.all([ // waits for the backends (below) to complete
-  ctcAlice.p.Alice({ // initialize a backend for alice.
-    ...Player('Alice'), //implement Alice's interact object here, instantiate the implementation once for Alice and once for Bob. These are the actual objects that wil be bound to interact in the Reach program.
-  wager: stdlib.parseCurrency(5), //defines Alice's wager as 5 units of the network token. This is an example of using a concrete value, rather than a fuction, in a participant interact interface.
+await Promise.all([
+  ctcAlice.p.Alice({
+    ...Player('Alice'),
+    wager: stdlib.parseCurrency(5),
+    deadline: 10,
   }),
-  ctcBob.p.Bob({ //initialize a backend for Bob
-    ...Player('Bob'), // implement Bob's interact object here
-  acceptWager: (amt) => { // define the acceptWager function.
-    console.log(`Bob accepts the wager of ${fmt(amt)}.`);
-  }
+  ctcBob.p.Bob({
+    ...Player('Bob'),
+    acceptWager: async (amt) => { // <-- async now
+      if ( Math.random() <= 0.5 ) {
+        for ( let i = 0; i < 10; i++ ) {
+          console.log(`  Bob takes his sweet time...`);
+          await stdlib.wait(1);
+        }
+      } else {
+        console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+      }
+    },
   }),
 ]);
 
