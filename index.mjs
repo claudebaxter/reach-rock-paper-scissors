4183f75256e9d1bd1c9b3789c7f257ed2c7953c1
e.baxter@dark-coin.io <e.baxter@dark-coin.io>
2022-07-26 20:03:39 -0500

    Fixing bugs

--- index.mjs
+++ index.mjs
@@ -18,12 +18,12 @@ const HAND = ['Rock', 'Paper', 'Scissors'];
 const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
 const Player = (Who) => ({
   ...stdlib.hasRandom,
-  getHand: async () => {
+  getHand: async () => { // <-- async now
     const hand = Math.floor(Math.random() * 3);
     console.log(`${Who} played ${HAND[hand]}`);
     if ( Math.random() <= 0.01 ) {
       for ( let i = 0; i < 10; i++ ) {
-        console.log(` ${Who} takes their sweet time sending it back...`);
+        console.log(`  ${Who} takes their sweet time sending it back...`);
         await stdlib.wait(1);
       }
     }
